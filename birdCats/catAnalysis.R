a16ccd28a66a388bf4bb0a7a67ec6e9f789dfb6b
bsevansunc <bsevans.unc@gmail.com>
2016-07-26 16:05:02 -0400

    added umf with covs and developed raw umf

--- birdCats/catAnalysis.R
+++ birdCats/catAnalysis.R
@@ -15,9 +15,12 @@ options(stringsAsFactors = F)
 # ---- Get data ----
 # ---------------------------------------------------------------------------------*
 
+catDataActivity <- read.csv('catDataActivity.csv') %>%
+  tbl_df
+
 catCam <- read.csv('catDataCamera.csv') %>%
   tbl_df %>%
-  filter(!is.na(CAT))
+  filter(!is.na(SPECIES))
 
 catSites <- read.csv('catSiteData.csv') %>%
   tbl_df %>%
@@ -25,7 +28,16 @@ catSites <- read.csv('catSiteData.csv') %>%
 
 catTransect <- read.csv('catDataTransect.csv') %>%
   tbl_df %>%
-  filter(!is.na(CAT))
+  select(-X) %>%
+  filter(!is.na(COUNT)) %>%
+  left_join(catDataActivity %>%
+              filter(ACTIVITY == 'Transect'),
+            by = c('SITE', 'VISIT')
+            ) %>%
+  # filter(!is.na(DISTANCE)) %>%
+  mutate(DISTANCE = as.numeric(DISTANCE),
+         VISIT = as.factor(VISIT)) %>%
+  arrange(SITE)
 
 catIncidental <- read.csv('catDataIncidental.csv') %>%
   tbl_df %>%
@@ -37,7 +49,34 @@ catIncidental <- read.csv('catDataIncidental.csv') %>%
 
 # Create an unmarked frame of distance data for the transect counts
 
-catTransectUmf <- formatDistData(catTransect, 
-                                distCol="DISTANCE",
-                                transectNameCol="site", 
-                                dist.breaks=seq(0, 50, by = 5))
+catTransectUmf <- formatDistData(
+  data.frame(catTransect), 
+  distCol="DISTANCE",
+  transectNameCol="SITE", 
+  dist.breaks=seq(0,50, by = 5) # ,
+  # occasionCol = 'VISIT'
+  )
+
+covs <- left_join(
+  catTransect,
+  read.csv('llData.csv') %>%
+    tbl_df %>%
+    arrange(site) %>%
+    dplyr::rename(SITE = site),
+  by = 'SITE'
+  ) %>%
+  select(-c(VISIT:TIME)) %>%
+  distinct
+  
+umfWithCovs <- unmarkedFrameDS(
+  y = as.matrix(catTransectUmf),
+  siteCovs = data.frame(covs),
+  survey = 'line',
+  dist.breaks=seq(0,50, by = 5),
+  tlength = rep(200, nrow(catTransectUmf)),
+  unitsIn = 'm'
+  )
+
+umfWithCovs %>% summary
+
+# 

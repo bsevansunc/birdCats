7b5a4765e5444468cc0ba9a04648fb0a78c7c882
Kevin Bennett <kevinfpbennett@gmail.com>
2016-08-13 22:28:45 -0400

    Ran more models and fixed convergence issue

--- birdCats/catAnalysis.R
+++ birdCats/catAnalysis.R
@@ -43,6 +43,7 @@ catTransect <- read.csv('catDataTransect.csv') %>%
 
 covs <- read.csv('covariateData.csv') %>%
   tbl_df %>%
+  mutate(medianIncomeAdj = medianIncome/1000) %>%
   arrange(site)
 
 catIncidental <- read.csv('catDataIncidental.csv') %>%
@@ -59,8 +60,8 @@ catTransectUmf <- formatDistData(
   data.frame(catTransect), 
   distCol="distance",
   transectNameCol="site", 
-  dist.breaks=seq(0,50, by = 5),
-  occasionCol = 'visit'
+  dist.breaks=seq(0,50, by = 5)#,
+#  occasionCol = 'visit'
   )
 
 # Get abundance covariates
@@ -112,11 +113,23 @@ longSitesWithObsCovs <- sitesWithObsCovs %>%
 
 # Create unmarkedFrameGDS object for gdistsamp
 
-umfWithCovs <- unmarkedFrameGDS(
+# umfWithCovs <- unmarkedFrameGDS(
+#   y =              as.matrix(catTransectUmf),
+#   siteCovs =       data.frame(sitesWithCovs),
+#   numPrimary =     1,                              
+#   yearlySiteCovs = data.frame(longSitesWithObsCovs[,-1]),
+#   survey =         'line',
+#   dist.breaks=     seq(0,50, by = 5),
+#   tlength =        rep(200, nrow(catTransectUmf)),
+#   unitsIn =        'm'
+#   )
+
+
+# Create unmarkedFrameDS object for distsamp
+
+umfWithCovs <- unmarkedFrameDS(
   y =              as.matrix(catTransectUmf),
-  siteCovs =       data.frame(sitesWithCovs),
-  numPrimary =     4,                              
-  yearlySiteCovs = longSitesWithObsCovs, #data.frame(sitesWithObsCovs),    # For use with unmarkedFrameGDS
+  siteCovs =       data.frame(sitesWithCovs),               
   survey =         'line',
   dist.breaks=     seq(0,50, by = 5),
   tlength =        rep(200, nrow(catTransectUmf)),
@@ -128,14 +141,21 @@ umfWithCovs <- unmarkedFrameGDS(
 # ----Transect model fitting----
 # ---------------------------------------------------------------------------------*
 
+# Not working
 
-gDensityNull <- gdistsamp(~1, ~1, ~1, umfWithCovs)
+# gDensityNull <- gdistsamp(~1, ~1, ~1, umfWithCovs)
+# 
+# gDensityImp <- gdistsamp(~imp, ~1, ~1, umfWithCovs)
+# 
+# gDetTemp <- gdistsamp(~1, ~1, ~temp, umfWithCovs)
+# 
+# gDensityEduC_detDew <- gdistsamp(~eduC, ~1, ~dew, umfWithCovs)
+# 
+# gDetDewTemp <- gdistsamp(~1, ~1, ~dew+temp, umfWithCovs)
+# 
+# gDensityImp_detDewTemp <- gdistsamp(~imp, ~1, ~dew+temp, umfWithCovs)
 
-gDensityImp <- gdistsamp(~imp, ~1, ~1, umfWithCovs)
 
-gDetTemp <- gdistsamp(~1, ~1, ~temp, umfWithCovs)
-
-gDensityEduC_detDew <- gdistsamp(~eduC, ~1, ~dew, umfWithCovs)
 
 
 # Null:
@@ -145,40 +165,100 @@ densityNull <- distsamp(~1 ~1, umfWithCovs)
 
 # Global:
 
-densityGlobal <-distsamp(~1 ~can +age + marred + eduC, umfWithCovs)
+densityGlobal <-distsamp(
+  ~1 ~can + hDensity + age + marred + eduC + medianIncomeAdj, umfWithCovs
+  )
 
 
 # Single covs:
 
-densityHumanDensity <- distsamp(~1 ~hDensity, umfWithCovs) # Model did not converge
+densityHDensity <- distsamp(
+  ~1 ~hDensity, umfWithCovs, control=list(
+    maxit=1000,trace=TRUE, REPORT=1
+    )
+  )
+
 densityImp <- distsamp(~1 ~imp, umfWithCovs)
+
 densityAge <- distsamp(~1 ~age, umfWithCovs)
-densityIncome <- distsamp(~1 ~medianIncome, umfWithCovs) # NaNs produced
+
+densityIncome <- distsamp(~1 ~medianIncomeAdj, umfWithCovs) # NaNs produced
+
 densityEduHS <- distsamp(~1 ~eduHS, umfWithCovs)
+
 densityEduC <- distsamp(~1 ~eduC, umfWithCovs)
+
 densityCan <- distsamp(~1 ~can, umfWithCovs)
+
 densityMar <- distsamp(~1 ~marred, umfWithCovs)
 
 
 
 # Additive:
 
-densityHDensityAge <- distsamp(~1 ~hDensity + age, umfWithCovs) # Model did not converge
+densityHDensityAge <- distsamp(
+  ~1 ~hDensity + age, umfWithCovs, control=list(
+    maxit=1000,trace=TRUE, REPORT=1
+    )
+  )
+
+densityHDensityEduC <- distsamp(
+  ~1 ~hDensity + eduC, umfWithCovs, control=list(
+    maxit=1700,trace=TRUE, REPORT=1
+    )
+  )
+
+densityHDensityCan <- distsamp(
+  ~1 ~hDensity + can, umfWithCovs, control=list(
+    maxit=1700,trace=TRUE, REPORT=1
+  )
+)
+
+densityHDensityCanEduC <- distsamp(
+  ~1 ~hDensity + can + eduC, umfWithCovs, control=list(
+    maxit=1700,trace=TRUE, REPORT=1
+  )
+)
+
 densityMarAge <- distsamp(~1 ~marred + age, umfWithCovs)
+
 densityCanImp <- distsamp(~1 ~can + imp, umfWithCovs)
-densityIncomeAge <- distsamp(~1 ~medianIncome + age, umfWithCovs) # NaNs produced
+
+densityIncomeAge <- distsamp(~1 ~medianIncomeAdj + age, umfWithCovs)
+
 densityAgeEduC <- distsamp(~1 ~eduC + age, umfWithCovs)
+
 densityCanAge <- distsamp(~1 ~can + age, umfWithCovs)
+
 densityCanEduC <- distsamp (~1 ~can + eduC, umfWithCovs)
+
 densityMarEduC <- distsamp(~1 ~marred + eduC, umfWithCovs)
+
+densityCanIncome <- distsamp(~1 ~can + medianIncomeAdj, umfWithCovs)
+
+densityIncomeEduC <- distsamp(~1 ~medianIncomeAdj + eduC, umfWithCovs)
+
+densityIncomeEduCCan <- distsamp(~1 ~medianIncomeAdj + eduC + can, umfWithCovs)
+
+
+
 
 # Interaction:
 
 densityImpIntAge <- distsamp(~1 ~imp*age, umfWithCovs) # NaNs produced
+
 densityCanIntAge <- distsamp(~1 ~can*age, umfWithCovs)
+
 densityImpIntHDensity <- distsamp(~1 ~imp*hDensity, umfWithCovs)
+
 densityMarIntAge <- distsamp(~1 ~marred*age, umfWithCovs)
-densityCanIntHDensity <- distsamp(~1 ~can*hDensity, umfWithCovs) # Model did not converge
+
+densityCanIntHDensity <- distsamp(
+  ~1 ~can*hDensity, umfWithCovs, control=list(
+    maxit=700,trace=TRUE, REPORT=1
+    )
+  ) 
+
 densityMarIntEduC <- distsamp(~1 ~marred*eduC, umfWithCovs)
 
 
@@ -230,8 +310,8 @@ camDetCovs <- read.csv('camDetCovs.csv') %>%
 
 camUmfWithCovs <- unmarkedFramePCount(
   umfCam[,-1],
-  siteCovs = camCovs,
-  obsCovs = camDetCovs
+  siteCovs = camCovs#,
+#  obsCovs = camDetCovs
   )
 
 
@@ -244,6 +324,9 @@ camUmfWithCovs <- unmarkedFramePCount(
 
 camDensityNull <- pcount(~1 ~1, camUmfWithCovs, K = 50)
 
+camDetDew <- pcount(~dewLow ~1, camUmfWithCovs, K = 50)
+camDetTempHigh <- pcount(~tempHigh ~1, camUmfWithCovs, K = 50)
+
 
 # Single abundance covariates
 
@@ -251,10 +334,22 @@ camDensityImp <- pcount(~1 ~imp, camUmfWithCovs, K = 50)
 camDensityCan <- pcount(~1 ~can, camUmfWithCovs, K = 50)
 camDensityhDensity <- pcount(~1 ~hDensity, camUmfWithCovs, K = 50)
 camDensityAge <- pcount(~1 ~age, camUmfWithCovs, K = 50)
-camDensityIncome <- pcount(~1 ~medianIncome, camUmfWithCovs, K = 50)
+camDensityIncome <- pcount(~1 ~medianIncomeAdj, camUmfWithCovs, K = 50)
 camDensityEduC <- pcount(~1 ~eduC, camUmfWithCovs, K = 50)
 camDensityEduHS <- pcount(~1 ~eduHS, camUmfWithCovs, K = 50)
 camDensityMar <- pcount(~1 ~marred, camUmfWithCovs, K = 50)
+
+
+# Additive
+
+camDensityCanEduC <- pcount(~1 ~can + eduC, camUmfWithCovs, K = 50)
+camDensityCanIncome <- pcount(~1 ~can + medianIncomeAdj, camUmfWithCovs, K = 50)
+camDensityAgeEduC <- pcount(~1 ~age + eduC, camUmfWithCovs, K = 50)
+camDensityAgeCan <- pcount(~1 ~age + can, camUmfWithCovs, K = 50)
+
+camDensityAgeCanEduC <- pcount(~1 ~age + can + eduC, camUmfWithCovs, K = 50)
+
+
 
 
 
